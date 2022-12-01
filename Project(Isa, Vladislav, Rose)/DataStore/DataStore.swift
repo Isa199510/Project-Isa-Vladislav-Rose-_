caac32951c1555286959d23aadcaedd1c2a80060
Isa199510 <isa.arbuhov@mail.ru>
2022-12-01 14:52:29 +0600

    add DataStore add Models

---
+++ Project(Isa, Vladislav, Rose)/DataStore/DataStore.swift
@@ -0,0 +1,14 @@
+//
+//  DataStore.swift
+//  Project(Isa, Vladislav, Rose)
+//
+//  Created by Иса on 01.12.2022.
+//
+
+import Foundation
+
+var shoppingList = [ShoppingLists]()
+
+func getShoppingList() -> [ShoppingLists] {
+    shoppingList
+}
