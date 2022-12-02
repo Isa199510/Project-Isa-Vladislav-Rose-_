ad0802659ac67d7a391518ee532581b247536f6c
Rose Khaldeeva <khalrose@mail.ru>
2022-12-02 21:56:21 +0600

    Add NewGroceryVC

---
+++ Project(Isa, Vladislav, Rose)/Rose/NewGroceryViewController.swift
@@ -0,0 +1,12 @@
+//
+//  NewGroceryViewController.swift
+//  Project(Isa, Vladislav, Rose)
+//
+//  Created by Rose on 02.12.2022.
+//
+
+import UIKit
+
+class NewGroceryViewController: ViewController {
+
+}
